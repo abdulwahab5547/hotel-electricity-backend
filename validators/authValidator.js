5c67e111d14740ced3b2f4e1bcf8d6e3f6f36174
New Username <abdulwahab5547@gmail.com>
2025-08-09 21:02:58 +0500

    updated

--- validators/authValidator.js
+++ validators/authValidator.js
@@ -1,9 +1,21 @@
 import { body } from 'express-validator';
 
 export const signupValidation = [
-  body('name').notEmpty().withMessage('Name is required'),
-  body('email').isEmail().withMessage('Enter a valid email'),
-  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
+  body('firstName')
+    .notEmpty()
+    .withMessage('First name is required'),
+  body('lastName')
+    .notEmpty()
+    .withMessage('Last name is required'),
+  body('buildingName')
+    .notEmpty()
+    .withMessage('Building name is required'),
+  body('email')
+    .isEmail()
+    .withMessage('Enter a valid email'),
+  body('password')
+    .isLength({ min: 6 })
+    .withMessage('Password must be at least 6 characters'),
 ];
 
 export const loginValidation = [
